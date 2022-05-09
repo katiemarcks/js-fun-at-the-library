1b694327f7ae459ae86d75bff12bfd6217ac5706
Katie Marcks <katiemarcks7@gmail.com>
2022-05-09 08:08:34 -0600

    Pass book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,43 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`;
+}
+
+function buildMainCharacter(name, age, pronouns) {
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  };
+}
+
+function saveReview(review, reviews) {
+  if (!reviews.includes(review)) {
+    reviews.push(review);
+  }
+}
+
+function calculatePageCount(title) {
+  return title.length * 20;
+}
+
+function writeBook(title, character, genre) {
+  return {
+    title: title,
+    mainCharacter: character,
+    genre: genre,
+    pageCount: calculatePageCount(title),
+  };
+}
+
+function editBook(book) {
+  book.pageCount = Math.round(book.pageCount - book.pageCount * 0.25);
 }
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
-}
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook,
+};

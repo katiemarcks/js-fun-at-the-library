fe550f14a6e07dc4d979058848fdd5c5a472e4dd
Katie Marcks <katiemarcks7@gmail.com>
2022-05-10 07:24:04 -0600

    Passed 9 tests on shelf.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,33 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.push(book);
+  }
+}
+
+function unshelfBook(title, shelf) {
+  let found = shelf.find((element) => element.title === title);
+  let index = shelf.indexOf(found);
+  shelf.splice(index, 1);
+}
+
+function listTitles(shelf) {
+  let str = "";
+  for (let i = 0; i < shelf.length; i++) {
+    const element = shelf[i];
+    if (i === shelf.length - 1) {
+      str += element.title;
+    } else {
+      str += element.title + ", ";
+    }
+  }
+  return str;
+}
+
+function searchShelf(shelf) {}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf,
 };

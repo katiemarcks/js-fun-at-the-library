f4032b74bdf44cb240e7b32e18bd5b702badb004
Katie Marcks <katiemarcks7@gmail.com>
2022-05-11 08:25:20 -0600

    Pass 5 tests on library.js and fail 1 test

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,23 @@
+function createLibrary(name) {
+  return {
+    name: name,
+    shelves: {
+      fantasy: [],
+      fiction: [],
+      nonFiction: [],
+    },
+  };
+}
 
+function addBook(library, book) {
+  let genre = book.genre;
+  let correctShelf = library.shelves.genre;
+  correctShelf = [];
+  correctShelf.push(book);
+}
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
